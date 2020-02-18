58df939e202cdb2471d34d41bc0767e8b2115a05
Myra-Grace <59889116+myra-grace@users.noreply.github.com>
2020-02-18 14:38:41 -0500

    workshop11

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ const exercisesP1 = require('./__workshop/exercisesP1');
 
 const PORT = 8000;
 
+const homepage = (req, res) => res.render('pages/homepage');
 const q6 = (req, res) => res.render('pages/question6');
 const q7 = (req, res) => res.render('pages/question7');
 const q8 = (req, res) => res.render('pages/question8');
 const q9 = (req, res) => res.render('pages/question9');
 const q10 = (req, res) => res.render('pages/question10');
+const fourOhFour = (req, res) => res.render('pages/fourOhFour');
 
 express()
     // This will give us will log more info to the console. see https://www.npmjs.com/package/morgan
@@ -36,16 +38,11 @@ express()
     .get('/question10', q10)
     
     // this serves up the homepage
-    .get('/', (req, res) => {
-        res.send('This is the homepage... it\'s empty :(');
-    })
+    .get('/', homepage)
 
     // this is our catch all endpoint. If a user navigates to any endpoint that is not
     // defined above, they get to see our 404 page.
-    .get('*', (req, res) => {
-        res.status(404);
-        res.send('404... This is not the page you are looking for.');
-    })
+    .get('*', fourOhFour)
 
     // Node spins up our server and sets it to listen on the PORT we defined above.
     .listen(PORT, () => console.log(`Listening on port ${PORT}`));
